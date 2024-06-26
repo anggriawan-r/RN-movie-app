40947f205d8542e70038bb66a8aeeb597911d6c0
MeilindaStephani <meilindastephaniee@gmail.com>
2024-06-26 20:12:05 +0700

    feat: add home screen fiture

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,17 +1,56 @@
 import React from 'react'
-import HomeStackNavigation from '../navigations/HomeStackNavigation'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import MovieDetail from '../navigations/MovieDetail'
+import { ScrollView, View, StatusBar, StyleSheet } from 'react-native'
+import type { MovieListProps } from '../types/app'
+import MovieList from '../components/MovieList'
 
-const Tab = createNativeStackNavigator()
+const movieLists: MovieListProps[] = [
+  {
+    title: 'Now Playing in Theater',
+    path: 'movie/now_playing?language=en-US&page=1',
+    coverType: 'backdrop',
+  },
+  {
+    title: 'Upcoming Movies',
+    path: 'movie/upcoming?language=en-US&page=1',
+    coverType: 'poster',
+  },
+  {
+    title: 'Top Rated Movies',
+    path: 'movie/top_rated?language=en-US&page=1',
+    coverType: 'poster',
+  },
+  {
+    title: 'Popular Movies',
+    path: 'movie/popular?language=en-US&page=1',
+    coverType: 'poster',
+  },
+]
 
-function Home(): JSX.Element {
+const Home = (): JSX.Element => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="HomeStack" component={HomeStackNavigation} />
-      <Tab.Screen name="MovieDetail" component={MovieDetail} />
-    </Tab.Navigator>
+    <ScrollView>
+      <View style={styles.container}>
+        {movieLists.map((movieList) => (
+          <MovieList
+            title={movieList.title}
+            path={movieList.path}
+            coverType={movieList.coverType}
+            key={movieList.title}
+          />
+        ))}
+        <StatusBar translucent={false} />
+      </View>
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: StatusBar.currentHeight ?? 32,
+    alignItems: 'center',
+    justifyContent: 'center',
+    rowGap: 16,
+  },
+})
 
 export default Home

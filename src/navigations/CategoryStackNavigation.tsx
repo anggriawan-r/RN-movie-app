eefaa7548d9766eb28125339a71fa10bf659a0dc
Anggriawan Rayzadmiko <anggriawan.net@gmail.com>
2024-06-29 02:31:46 +0700

    feat: add navigation to movie detail from category screen

--- src/navigations/CategoryStackNavigation.tsx
+++ src/navigations/CategoryStackNavigation.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Search from '../screens/Search'
 import CategorySearchResult from '../screens/CategorySearchResult'
+import MovieDetail from '../screens/MovieDetail'
 
 const Stack = createNativeStackNavigator()
 
@@ -17,6 +18,11 @@ const CategoryStackNavigation = () => {
         name="SearchResult"
         component={CategorySearchResult}
         options={{ title: 'Search Result' }}
+      />
+      <Stack.Screen
+        name="MovieDetail"
+        component={MovieDetail}
+        options={{ title: 'Movie Detail' }}
       />
     </Stack.Navigator>
   )

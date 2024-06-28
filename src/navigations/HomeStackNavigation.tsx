ddc8112d17db490026b113d0decbfa543fa4db5d
Anggriawan Rayzadmiko <anggriawan.net@gmail.com>
2024-06-28 22:56:42 +0700

    fix: fix conflict

--- src/navigations/HomeStackNavigation.tsx
+++ src/navigations/HomeStackNavigation.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from '../screens/Home'
 import MovieDetail from './MovieDetail'
@@ -9,12 +8,10 @@ const Stack = createNativeStackNavigator<RootStackParamList>()
 
 const HomeStackNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="MovieDetail" component={MovieDetail} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="MovieDetail" component={MovieDetail} />
+    </Stack.Navigator>
   )
 }
 
